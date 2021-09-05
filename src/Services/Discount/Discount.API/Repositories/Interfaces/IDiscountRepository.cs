e131bbefd8bb4a83f1b8254f52c2a586cbbfe9a8
João Kuntz <joaoluiskuntz@gmail.com>
2021-09-05 20:02:06 -0300

    Implementation of Discount.API

---
+++ src/Services/Discount/Discount.API/Repositories/Interfaces/IDiscountRepository.cs
@@ -0,0 +1,16 @@
+﻿using Discount.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.API.Repositories.Interfaces
+{
+    public interface IDiscountRepository
+    {
+        public Task<Coupon> GetDiscount(string productName);
+        public Task<bool> CreateDiscount(Coupon coupon);
+        public Task<bool> UpdateDiscount(Coupon coupon);
+        public Task<bool> DeleteDiscount(string productName);
+    }
+}
